4a21dcff0d979c960b59d77f9945dd1de377a530
William Ndongmo <williamndongmo88@gmail.com>
2025-07-25 00:06:18 +0200

    v1-prod: create user and send order mail

--- src/main/java/will/dev/artisan_des_saveurs/ArtisanDesSaveursApplication.java
+++ src/main/java/will/dev/artisan_des_saveurs/ArtisanDesSaveursApplication.java
@@ -36,8 +36,8 @@ public class ArtisanDesSaveursApplication {
 	public void testDBEnvVars() {
 		System.out.println("DB URL: " + System.getProperty("DATABASE_URL"));
 		System.out.println("DB USER: " + System.getProperty("DATABASE_USERNAME"));
-		System.out.println("MAIL USERNAME: " + System.getProperty("MAIL_USERNAME"));
-		System.out.println("MAIL PASSWORD: " + System.getProperty("MAIL_PASSWORD"));
+		System.out.println("MAIL_USERNAME = " + System.getenv("MAIL_USERNAME"));
+		System.out.println("MAIL_PASSWORD = " + System.getenv("MAIL_PASSWORD"));
 //		System.out.println("accountSid = " + System.getProperty("TWILIO_ACCOUNT_SID"));
 //		System.out.println("authToken = " + System.getProperty("TWILIO_AUTH_TOKEN"));
 //		System.out.println("fromPhoneNumber = " + System.getProperty("TWILIO_WHATSAPP_FROM"));
