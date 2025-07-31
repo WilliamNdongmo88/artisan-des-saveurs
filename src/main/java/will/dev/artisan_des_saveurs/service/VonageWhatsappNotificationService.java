d7c9dbba368a23311b6ec32e6838c18eb4f5727d
William Ndongmo <williamndongmo88@gmail.com>
2025-07-31 02:37:53 +0200

    v1-prod: MAJ classes  OrderService et VonageWhatsapp...

--- src/main/java/will/dev/artisan_des_saveurs/service/VonageWhatsappNotificationService.java
+++ src/main/java/will/dev/artisan_des_saveurs/service/VonageWhatsappNotificationService.java
@@ -1,13 +1,10 @@
 package will.dev.artisan_des_saveurs.service;
-//import com.vonage.client.VonageClient;
-//import com.vonage.client.sms.MessageStatus;
-//import com.vonage.client.sms.SmsSubmissionResponse;
-//import com.vonage.client.sms.messages.TextMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import will.dev.artisan_des_saveurs.entity.ContactRequest;
 import will.dev.artisan_des_saveurs.entity.User;
 
 import java.util.Map;
@@ -27,9 +24,7 @@ public class VonageWhatsappNotificationService {
     @Value("${app.company.whatsappNumber}")
     private String companyNumber;
 
-//    private final VonageClient vonageClient;
-
-    public void sendWhatsappMessageToCustomer(User savedUser, String customerMessage) {
+    public void sendWhatsappMessageToCustomer(User savedUser, ContactRequest contactRequest) {
         String url = "https://messages-sandbox.nexmo.com/v0.1/messages";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -41,25 +36,13 @@ public class VonageWhatsappNotificationService {
         Map<String, Object> body = Map.of(
                 "from", Map.of("type", "whatsapp", "number", fromNumber),
                 "to", Map.of("type", "whatsapp", "number", companyNumber),
-                "message", Map.of("content", Map.of("type", "text", "text", customerMessage))
+                "message", Map.of("content", Map.of("type", "text", "text", contactRequest.getMessage()))
         );
 
         HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
 
         restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
     }
-
-//    public void sendSmsToCustomer(User savedUser, String customerMessage) {
-//        TextMessage message = new TextMessage("VonageSMS", companyNumber, customerMessage);
-//
-//        SmsSubmissionResponse response = vonageClient.getSmsClient().submitMessage(message);
-//
-//        if (response.getMessages().get(0).getStatus() == MessageStatus.OK) {
-//            ResponseEntity.ok("Message envoyé avec succès.");
-//        } else {
-//            ResponseEntity.status(500).body("Erreur d'envoi : " + response.getMessages().get(0).getErrorText());
-//        }
-//    }
 }
 
 
