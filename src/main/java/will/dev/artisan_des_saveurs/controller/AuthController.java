88efebc52a81f0769e7492a9b1f165246ec3b3fa
William Ndongmo <williamndongmo88@gmail.com>
2025-08-22 01:47:51 +0200

    Logique: généreration d'une URL de l'image  vers Nginx

--- src/main/java/will/dev/artisan_des_saveurs/controller/AuthController.java
+++ src/main/java/will/dev/artisan_des_saveurs/controller/AuthController.java
@@ -20,7 +20,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
     AuthenticationManager authenticationManager;
