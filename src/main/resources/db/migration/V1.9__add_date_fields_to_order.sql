3608a4e1cc94e1db90f3ae7c3d5ec86b1019b6d9
William Ndongmo <williamndongmo88@gmail.com>
2025-08-23 20:47:13 +0200

    Logique: historique de commande

--- src/main/resources/db/migration/V1.9__add_date_fields_to_order.sql
+++ src/main/resources/db/migration/V1.9__add_date_fields_to_order.sql
@@ -1,5 +1,10 @@
 ALTER TABLE orders
-ADD COLUMN delivered ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled') DEFAULT 'processing';
+ALTER COLUMN delivered TYPE VARCHAR(20)
+USING CASE
+    WHEN delivered = TRUE THEN 'processing'
+    WHEN delivered = FALSE THEN 'delivered'
+    ELSE NULL
+END;
 
 ALTER TABLE orders
 ADD COLUMN created_at TIMESTAMP;
