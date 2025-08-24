832a2ef0a88d68cc4aef6a51af325ecd8c96aec4
William Ndongmo <williamndongmo88@gmail.com>
2025-08-24 02:49:39 +0200

    Logique: historique de commande

--- src/main/java/will/dev/artisan_des_saveurs/dto/order/OrdersResponse.java
+++ src/main/java/will/dev/artisan_des_saveurs/dto/order/OrdersResponse.java
@@ -13,8 +13,8 @@ public class OrdersResponse {
     private double total;
     private boolean freeShipping;
     private String delivered;
-    //private LocalDateTime createAt;
-    private String createAt;
+    private LocalDateTime createdAt;
+    //private String createdAt;
     private Long userid;
     private ProductItemDTO productItem;
 }
