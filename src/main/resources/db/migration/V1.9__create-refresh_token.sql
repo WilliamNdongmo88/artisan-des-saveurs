838376ff93ffb6530a21444b153d5f9b455cac08
William Ndongmo <williamndongmo88@gmail.com>
2025-08-26 16:55:32 +0200

    Logique: Implémentation du refresh token

--- src/main/resources/db/migration/V1.9__create-refresh_token.sql
+++ src/main/resources/db/migration/V1.9__create-refresh_token.sql
@@ -10,10 +10,3 @@ CREATE TABLE refresh_tokens (
 CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(token);
 CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens(user_id);
 CREATE INDEX idx_refresh_tokens_expiry_date ON refresh_tokens(expiry_date);
-
--- Commentaires pour documentation
-COMMENT ON TABLE refresh_tokens IS 'Table pour stocker les refresh tokens JWT';
-COMMENT ON COLUMN refresh_tokens.id IS 'Identifiant unique du refresh token';
-COMMENT ON COLUMN refresh_tokens.user_id IS 'Référence vers l\'utilisateur propriétaire du token';
-COMMENT ON COLUMN refresh_tokens.token IS 'Valeur unique du refresh token (UUID)';
-COMMENT ON COLUMN refresh_tokens.expiry_date IS 'Date d\'expiration du refresh token';
